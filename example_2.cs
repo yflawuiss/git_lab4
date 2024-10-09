e7419d6544cd81da8e3fd27d3217a43c0e025851
Veretekha Dimitriy <skmmlt@gmail.com>
2024-10-09 19:45:39 +0300

    commit examples
    Коміт з опрацьованими прикладами лабораторної роботи №4

---
+++ example_2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace example_2
+{
+    class Program
+    {
+        // Опис структури всередині класу але поза методами
+        struct Lud
+        {
+            // Поля
+            public string f;  // Прізвище 
+            public int gr;  // Рік народження
+            public char p;  // Стать (ч/ж)
+
+            // Метод
+            public string Vivod()
+            {
+                string s = f + " " + Convert.ToString(gr) + " " + Convert.ToString(p);
+                return s;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Створення екземпляру структури
+            Lud lud = new Lud();
+
+            // Змінні для зберігання значень
+            string fLud;
+            int grLud;
+            char pLud;
+
+            // Зчитування даних від користувача
+            Console.Write("Введiть прiзвище: "); fLud = Console.ReadLine();
+            
+            Console.Write("Введiть рiк народження: ");
+            while (!int.TryParse(Console.ReadLine(), out grLud) || grLud < 1900 || grLud > DateTime.Now.Year)
+            {
+                Console.Write("Невiрний рiк. Спробуйте ще раз: ");
+            }
+
+            Console.Write("Введiть стать (ч/ж): ");
+            while (true)
+            {
+                pLud = Convert.ToChar(Console.ReadLine());
+                if (pLud == 'ч' || pLud == 'ж')
+                    break;
+                Console.Write("Невiрна стать. Введiть «ч» або «ж»: ");
+            }
+
+            // Присвоєння значень полям структури
+            lud.f = fLud;
+            lud.gr = grLud;
+            lud.p = pLud;
+
+            // Виведення інформації
+            Console.WriteLine($"Введенi данi: {lud.Vivod()}");
+        }
+    }
+}
---
+++ example_3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace example_3
+{
+    class Program
+    {
+        
+        struct Lud  // Опис структури
+        {
+            // Поля
+            public string f;  // Прізвище 
+            public int gr;  // Рік народження
+            public char p;  // Стать (ч/ж)
+
+            // Метод
+            public string Vivod()
+            {
+                string s = f + " " + Convert.ToString(gr) + " " + Convert.ToString(p);
+                return s;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Список студентів
+            List<Lud> students = new List<Lud>();
+            // Опис змінної для введення, без іниціалізації полів
+            Lud stud;
+            // Використовуємо конструктор, для іниціалізації полів
+            // Опис змінної для запам'ятовування наймолодшого
+            Lud mx = new Lud();
+            int n = 4;  // Кількість студентів
+            Console.WriteLine("Введiть данi: ");
+            // Ввод і заповнення списку
+            for (int i = 0; i < n; i++)
+            {
+                // Заповнення полей структури
+                stud.f = Console.ReadLine();
+                stud.gr = Convert.ToInt32(Console.ReadLine());
+                stud.p = Convert.ToChar(Console.ReadLine());
+                students.Add(stud);
+            }
+            Console.WriteLine("Список студентiв:");
+
+            // Виведення списку на екран
+            for (int i = 0; i < n; i++)
+            {
+                // Застосування методу структури
+                Console.WriteLine(students[i].Vivod());
+            }
+
+            // Перегляд списка і надходження наймолодшої студентки
+            for (int i = 0; i < n; i++)
+            {
+                if (students[i].p == 'ж' &&
+                   (students[i].gr == 0 || students[i].gr > mx.gr)) mx = students[i];
+            }
+
+            // Виведення на екран
+            if (mx.gr != 0)
+                Console.WriteLine("\nНаймолодша студентка: {0} {1} {2}", mx.f, mx.gr, mx.p);
+            else Console.WriteLine("\nНемає дiвчин.");
+            Console.ReadKey();
+        }
+    }
+}
